9b3dc9d8719fccac08b29cc789a9ba96a5d02b13
urape <mydevelop.mkmk@gmail.com>
2019-10-18 11:30:47 +0900

    add production seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        if (App::environment() === 'production') {
+            $this->call('ProductionSeeder');
+        } else {
+            $this->call('LocalSeeder');
+        }
     }
 }
